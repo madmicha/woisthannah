6a5be020f66e60f5d1a20f1246b93c3c87577f0c
madmicha <ubuntu@michael-reinartz.de>
2018-02-11 22:23:52 +0100

    Bug Fix: Script did  not work, had to modify date parser  (German date format (Mrz)  not supported)

--- hannah/spiders/hannah_test.py
+++ hannah/spiders/hannah_test.py
@@ -1,33 +1,71 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import locale
+import time
 from datetime import datetime
-loc= locale.setlocale(locale.LC_ALL,("de_DE.UTF-8"))
+#import datetime
+locale.setlocale(locale.LC_ALL,("de_DE.utf8"))
+#Helper Functions to parse the given date-format (German Months, without explicit year given )
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
 
+def parse_future(timestr, **parse_kwargs):
+    """Same as dateutil.parser.parse() but only returns future dates."""
+    now = datetime.now()
+    try:
+        dt = parser.parse(timestr, parserinfo=GermanParserInfo(), **parse_kwargs)
+    except ValueError:
+        pass
+    if dt > now: # original date is in future
+        pass
+    else:
+        dt += relativedelta(years=+1)
+    if dt > now: # future date is one year later
+        pass
+    else:
+        print("No Future date found")
+    return dt
+
+class GermanParserInfo(parser.parserinfo):
+    MONTHS = [
+        ('Jan'),('Feb'),('Mrz'),('Apr'),('Mai'),('Jun'),('Jul'),('Aug'),('Sep'),('Okt'),('Nov'),('Dez'),
+    ]
+#testdate = datetime.date(2013,3,3)
+#print(testdate.strftime("%d.%b %Y"))
 class HannahSpider(scrapy.Spider):
     name = 'hannah_v1'
     allowed_domains = ['hannah-lastenrad.de']
     start_urls_prefix = "https://www.hannah-lastenrad.de/cb-items/hannah-"
     start_urls = []
-    for i in range(1,18):
+    for i in range(1,19):
         start_url = ''.join([start_urls_prefix,str(i),'//'])
         start_urls.append(start_url)
     #start_urls = ['http://https://www.hannah-lastenrad.de//']
 
     def parse(self, response):
         print "URL: " + response.url
-        print(response.xpath('//li[contains(@class, "bookable")]//span/text()').extract())
+        #print(response.xpath('//li[contains(@class, "bookable")]//span/text()').extract())
         bookable_dates_iterator = iter(response.xpath('//li[contains(@class, "bookable")]//span/text()').extract())
-        bookable_dates = [c.zfill(3) +next(bookable_dates_iterator,'') for c in bookable_dates_iterator]
+        bookable_dates = [c.zfill(3) +' ' +next(bookable_dates_iterator,'') for c in bookable_dates_iterator]
+        bookable_dates = [''.join([t,'2018']) for t in bookable_dates]
         print(list(bookable_dates))
+        try:
+            print(bookable_dates)
+            print(bookable_dates_iterator)
+            bookable_date_parsed = [parse_future(t) for t in bookable_dates]
+            print(bookable_date_parsed)
+        except:
+            print("mooep")
+        time.sleep(1)
+        
         location_timeframend = [x[-8:] for x in response.xpath("//span[@class='cb-date']/text()").extract()]
      
         yield {
         'hannah_name': response.xpath('//h1/text()').extract_first(),
         'location_names': [x.strip() for x in response.xpath("//div[@class='cb-location-name cb-big']/text()").extract()],
         'location_gmaps_links': [x.strip() for x in response.xpath("//div[@class='cb-address cb-row']/a/@href").extract()],
-        'location_timeframe': [datetime.strptime(t,'%d.%m.%y') for t in [x.strip() for x in location_timeframend]],
-        'bookable_dates': [datetime.strptime(''.join([t,'2017']),'%d.%b %Y') for t in bookable_dates]
+        'location_timeframe': [parse_future(t) for t in [x.strip() for x in location_timeframend]],
+        'bookable_dates': [parse_future(t) for t in bookable_dates]
         }
         pass
         
--- tweet_woisthannah.py
+++ tweet_woisthannah.py
@@ -15,25 +15,27 @@ def nearest_date(items):
     return min(items, key=lambda x: abs(x - datetime.today()))
 print(api.VerifyCredentials())
 
+#empty_jsonline    
+with open('hannah_bookable_dates.jl', 'w'): pass	
+
 #crawl
-
-
 settings = get_project_settings()
 settings.overrides['FEED_FORMAT'] = 'jsonlines'
 settings.overrides['FEED_URI'] = 'hannah_bookable_dates.jl'
 
 process = CrawlerProcess(settings)
 
-# 'followall' is the name of one of the spiders of the project.
+# hannah_v1 is the name of one of the spiders of the project.
 process.crawl('hannah_v1', domain='hannah-lastenrad.de')
 process.start() # the script will block here until the crawling is finished
 
 time.sleep(1)
 
-#load jsonlines
+#load jsonlines containing scraped data
 hannah_data = []
 earliest_bookable_dates =[]
 not_bookable_hannahs = []
+
 
 with open('hannah_bookable_dates.jl', 'r') as f:
     for line in f:
@@ -43,10 +45,12 @@ with open('hannah_bookable_dates.jl', 'r') as f:
         else:
             not_bookable_hannahs.append(json.loads(line)['hannah_name'])
         hannah_data.append(json.loads(line))
-#        
-with open('hannah_bookable_dates.jl', 'w'): pass	
+
+
+
 print(earliest_bookable_dates)
 print(not_bookable_hannahs)
+
 next_bookable_hannahs =[]
 for line in hannah_data:
 	if line['bookable_dates'] == []:
@@ -57,11 +61,28 @@ for line in hannah_data:
 if nearest_date(earliest_bookable_dates).date() == datetime.today().date():
     if len(next_bookable_hannahs)==1:
         status_text = "".join(["Heute gibt es noch ein kostenloses #Lastenrad in #Hannover"]+next_bookable_hannahs+[' https://www.hannah-lastenrad.de/cb-items/hannah-',next_bookable_hannahs[0].split('nnah')[1]])
+    if len(next_bookable_hannahs) != 1 and len(next_bookable_hannahs) < 6:
+        today_bookable_hannahs =[]
+        for bookable_hannah in next_bookable_hannahs:
+            today_bookable_hannahs.append(bookable_hannah)
+            today_bookable_hannahs.append(": https://www.hannah-lastenrad.de/cb-items/hannah-")
+            today_bookable_hannahs.append(bookable_hannah.split('nnah')[1])
+            today_bookable_hannahs.append("\n")
+        status_text = "".join(["Heute sind noch ",str(len(next_bookable_hannahs))," kostenlose #Lastenrad frei: \n"]+today_bookable_hannahs)
     else:
         status_text = "".join(["Heute sind ",str(len(next_bookable_hannahs))," #Lastenrad in #Hannover buchbar:"]+next_bookable_hannahs+[" https://www.hannah-lastenrad.de"])
 else:
     if len(next_bookable_hannahs)==1:
         status_text = "".join(["Ein kostenloses #Lastenrad gibt es wieder am ",datetime.strftime(nearest_date(earliest_bookable_dates),'%d.%m.%y')," in #Hannover:"]+next_bookable_hannahs+[' https://www.hannah-lastenrad.de/cb-items/hannah-',next_bookable_hannahs[0].split('annah')[1]])
+    if len(next_bookable_hannahs) != 1 and len(next_bookable_hannahs) < 6:
+        someday_bookable_hannahs =[]
+        for bookable_hannah in next_bookable_hannahs:
+            print(bookable_hannah)
+            someday_bookable_hannahs.append(bookable_hannah)
+            someday_bookable_hannahs.append(": https://www.hannah-lastenrad.de/cb-items/hannah-")
+            someday_bookable_hannahs.append(bookable_hannah.split('nnah')[1])
+            someday_bookable_hannahs.append("\n")
+        status_text = "".join(["Schnappt dir ein #Lastenrad von ",str(len(next_bookable_hannahs))," in #Hannover wieder ab ",datetime.strftime(nearest_date(earliest_bookable_dates),'%d.%m') ,": \n"]+someday_bookable_hannahs)
     else:
         status_text = "".join(["Schnapp dir ein #Lastenrad von ",str(len(next_bookable_hannahs))," in #Hannover wieder ab ",datetime.strftime(nearest_date(earliest_bookable_dates),'%d.%m'),":"]+next_bookable_hannahs+[" https://www.hannah-lastenrad.de"])
 print(status_text)
@@ -69,7 +90,7 @@ print(status_text)
 #message_text = "".join(["Tweetbot sagt Guten Morgen am ",datetime.strftime(datetime.today(),"%d.%m.%.y"),status_text])
 #message = api.PostDirectMessage(u'madmicha',message_text)
 #Post Status
-status = api.PostUpdate(status_text)
+status = api.PostUpdate(status_text,verify_status_length=False)
 print(status.text)
 
 #empty jl-file
